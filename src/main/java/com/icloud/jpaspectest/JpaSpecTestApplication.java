542a97c8204aed9681d9676eb1e1afb6820a1aba
hope <hope@gravylab.co.kr>
2022-05-17 01:09:33 +0900

    initial commit

---
+++ src/main/java/com/icloud/jpaspectest/JpaSpecTestApplication.java
@@ -0,0 +1,13 @@
+package com.icloud.jpaspectest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpecTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icloud/jpaspectest/JpaSpecTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.jpaspectest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpecTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
